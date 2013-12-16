7b54f7640b7ad55d0d9052c665e7f22d486fe461
sarony <saron.yitbarek@gmail.com>
2013-12-15 23:37:47 -0500

    youtube, treehouse done. halfway codeschool. finish up .getScript to talk to app

---
+++ TODO.md
@@ -0,0 +1,10 @@
+#- href with hardcoded link
+#- have javascript that can find the video link
+---> link available for treehouse, youtube, codeschool
+- have javascript that can find the video link from any page
+- have the bookmarklet be able to check if you're logged in first
+#- host the js on bluehost
+- link the bookmarklet to noted.com with .getScript (might need to push to heroku?, work through local host stuff)
+
+***
+- doesn't work for RailsForZombies.org
--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,3 +1,54 @@
-;(function(){
-  alert('From Github yo!');
-})(window);
+// ;(function(){
+//   alert('From Github yo!');
+// })(window);
+
+(function(){
+
+  // the minimum version of jQuery we want
+  var v = "1.3.2";
+
+  // check prior inclusion and version
+  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+    var done = false;
+    var script = document.createElement("script");
+    script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+    script.onload = script.onreadystatechange = function(){
+      if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+        done = true;
+        initMyBookmarklet();
+      }
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+  } else {
+    initMyBookmarklet();
+  }
+  
+  function initMyBookmarklet() {
+    $.getScript('127.0.0.1:3000/user');
+    var videoLink = ""
+    // from code school
+    if(window.location.href.search("codeschool")>=0){
+      var codeSchool = []
+      $("source").each(function(){
+        if($(this)[0].getAttribute("type")=="video/mp4"){
+          codeSchool.push($(this)[0].getAttribute("src"));
+        };
+      });
+      videoLink=codeSchool[0];
+    }
+    else if(window.location.href.search("youtube")>=0){
+      // from youtube
+      videoLink = $("meta[property='og:video']")[0].getAttribute('content');
+    }
+    else if(window.location.href.search("treehouse")>=0){
+      // from treehouse
+      $("source").each(function(){
+      if($(this)[0].getAttribute("type")=="video/mp4"){
+        videoLink = $(this)[0].getAttribute("src");
+        };
+      });
+    };
+    alert(videoLink);
+  }
+
+})();
